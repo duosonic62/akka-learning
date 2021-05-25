1111452a9f118b8f94d6496537a1778b07cb51aa
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-05-25 22:52:42 +0900

    add silent actor test

--- chapter-3/akka-test-kit/src/test/scala/com/akka-test/silent/SilentActor01Test.scala
+++ chapter-3/akka-test-kit/src/test/scala/com/akka-test/silent/SilentActor01Test.scala
@@ -1,10 +1,13 @@
-package com.`akka-test`
+package com.`akka-test`.silent
 
 import akka.testkit.TestKit
 import akka.actor.ActorSystem
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatest.matchers.must.Matchers
 import com.goticks.StopSystemAfterAll
+import akka.testkit.TestActor
+import akka.testkit.TestActorRef
+import akka.actor.Props
 
 class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
     with AnyWordSpecLike
@@ -13,12 +16,22 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
     
         // SilentActor
         "A Silent Actor" must {
-            "change state when if trcives message, single threaded" in {
-                fail("not implemented yet")
+            "change state when if recieves message, single threaded" in {
+                import SilentActor._
+                val silentActor = TestActorRef[SilentActor]
+                
+                silentActor ! SilentMessage("whisper")
+                silentActor.underlyingActor.state must contain("whisper")
             }
 
-            "change state when if trcives message, multi threaded" in {
-                fail("not implemented yet")
+            "change state when if recieves message, multi threaded" in {
+                import SilentActor._
+                val silentActor = system.actorOf(Props[SilentActor], "s3")
+                silentActor ! SilentMessage("whisper 1")
+                silentActor ! SilentMessage("whisper 2")
+
+                silentActor ! GetState(testActor) // testKitに含まれているtestActorにメッセージを送らせちゃう
+                expectMsg(Vector("whisper 1", "whisper 2"))
             }
         }
 }
