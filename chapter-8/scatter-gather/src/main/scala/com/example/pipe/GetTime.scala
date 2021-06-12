72285cfa9e8858c9c613d06dee2c207542f9cbb2
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-06-12 09:37:34 +0900

    add recipient list test

--- chapter-8/scatter-gather/src/main/scala/com/example/pipe/GetTime.scala
+++ chapter-8/scatter-gather/src/main/scala/com/example/pipe/GetTime.scala
@@ -2,7 +2,7 @@ package com.example.pipe
 
 import akka.actor.{Actor, ActorRef}
 
-import java.time. LocalDateTime}
+import java.time. LocalDateTime
 
 class GetTime(pipe: ActorRef) extends Actor {
   override def receive: Receive = {
