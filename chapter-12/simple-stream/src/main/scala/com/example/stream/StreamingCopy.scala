2d7d4ab18224c3aa8a70489087368f627db9d958
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-06-25 21:12:52 +0900

    add run graph

--- chapter-12/simple-stream/src/main/scala/com/example/stream/StreamingCopy.scala
+++ chapter-12/simple-stream/src/main/scala/com/example/stream/StreamingCopy.scala
@@ -1,19 +1,27 @@
 package com.example.stream
 
+import akka.actor.ActorSystem
 import akka.stream.IOResult
-import akka.stream.scaladsl.{FileIO, Sink, Source, RunnableGraph}
+import akka.stream.scaladsl.{FileIO, RunnableGraph, Sink, Source}
 import akka.util.ByteString
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Paths
 import java.nio.file.StandardOpenOption._
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
 object StreamingCopy extends App {
-  val inputPath = Paths.get("")
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  val inputPath = Paths.get("sample.txt")
   val source: Source[ByteString, Future[IOResult]] = FileIO.fromPath(inputPath)
 
-  val outputFile = Paths.get("")
+  val outputFile = Paths.get("sample_copy.txt")
   val sink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(outputFile, Set(CREATE, WRITE, APPEND))
 
   val runnableGraph: RunnableGraph[Future[IOResult]] = source.to(sink)
+  runnableGraph.run().foreach { result =>
+    println(s"${result.status}, ${result.count} bytes read.")
+    system.terminate()
+  }
 }
