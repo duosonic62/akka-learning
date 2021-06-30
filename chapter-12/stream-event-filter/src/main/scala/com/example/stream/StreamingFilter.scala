ecd117001a7098e85416ac218bc979f47c5ba82f
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-06-30 20:35:26 +0900

    add bidi flow sample

--- chapter-12/stream-event-filter/src/main/scala/com/example/stream/StreamingFilter.scala
+++ chapter-12/stream-event-filter/src/main/scala/com/example/stream/StreamingFilter.scala
@@ -2,18 +2,14 @@ package com.example.stream
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.actor.FSM.Failure
-import akka.actor.TypedActor.Supervisor
+import akka.stream.scaladsl._
 import akka.stream.{ActorAttributes, IOResult, Supervision}
 import akka.util.ByteString
+import com.example.stream.LogStreamProcessor.LogParseException
 
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption._
 import scala.concurrent.{ExecutionContextExecutor, Future}
-import akka.stream.scaladsl._
-import com.example.stream.LogStreamProcessor.LogParseException
-
-import scala.util.Success
 
 
 object StreamingFilter extends App with EventMarshalling {
@@ -28,27 +24,25 @@ object StreamingFilter extends App with EventMarshalling {
 
   val maxLine = 512
 
-  val frame: Flow[ByteString, String, NotUsed] = Framing.delimiter(ByteString("\n"), maxLine)
-    .map { str => str.decodeString("UTF8") }
-
   val decider: Supervision.Decider = {
     case _: LogParseException => Supervision.Resume
     case _ => Supervision.Stop
   }
-  val parse: Flow[String, Event, NotUsed] = Flow[String].map(LogStreamProcessor.parseLineEx)
+  val inFlow: Flow[ByteString, Event, NotUsed] = Framing.delimiter(ByteString("\n"), maxLine)
+    .map { str => str.decodeString("UTF8") }
+    .map(LogStreamProcessor.parseLineEx)
     .collect { case Some(e) => e }
     .withAttributes(ActorAttributes.supervisionStrategy(decider))
 
-  val filter: Flow[Event, Event, NotUsed] = Flow[Event].filter(_.state == Ok)
-
-  val serialize: Flow[Event, ByteString, NotUsed] = Flow[Event].map { event =>
+  val outFlow: Flow[Event, ByteString, NotUsed] = Flow[Event].map { event =>
     ByteString(event.toString + "\n")
   }
 
+  val bidiFlow = BidiFlow.fromFlows(inFlow, outFlow)
+
+  val filter: Flow[Event, Event, NotUsed] = Flow[Event].filter(_.state == Ok)
   val composedFlow: Flow[ByteString, ByteString, NotUsed] =
-    frame.via(parse)
-      .via(filter)
-      .via(serialize)
+    bidiFlow.join(filter)
 
   val runnableGraph: RunnableGraph[Future[IOResult]] = source.via(composedFlow).toMat(sink)(Keep.right)
 
